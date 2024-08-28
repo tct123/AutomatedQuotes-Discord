2cc1907762bf019bc6f22e5478d082bd3b14bd72
tct123 <42028373+tct123@users.noreply.github.com>
2024-08-28 18:59:00 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,11 @@
 import discord
 import os
 import dotenv as dv
-import asyncio
+from quotesgeneratorapi_wrapper.quotesgenerator import getQuotes
 
 dv.load_dotenv()
 TOKEN_DISCORD = os.getenv("TOKEN_DISCORD")
+APININJA_TOKEN = os.getenv("APININJA_TOKEN")
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -21,7 +22,7 @@ async def on_message(message):
     if message.author == client.user:
         return
     if message.content.startswith("$hello"):
-        await message.channel.send("Hello!")
+        await message.channel.send(getQuotes(APININJA_TOKEN))
 
 
 client.run(token=TOKEN_DISCORD)
